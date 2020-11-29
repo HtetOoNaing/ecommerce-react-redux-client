f958738708ef7ef49215a54e86a3a20a2757dd08
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-11-29 22:34:52 +0630

    add button and container components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/style.css';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// redux
 import { Provider } from 'react-redux';
 import store from './data/store';
 
 ReactDOM.render(
   <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
     <App />
   </Provider>,
   document.getElementById('root')
