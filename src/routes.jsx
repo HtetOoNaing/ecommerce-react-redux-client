31ea69711f28b82485e3c31df7b69d5274946c97
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-12-01 21:19:00 +0630

    add login route and finish login

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/navbar/navbar.component';
 import Home from './screens/Home';
+import Login from './screens/Login';
 import Register from './screens/Register';
 
 const Routes = () => {
@@ -12,6 +13,7 @@ const Routes = () => {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/register" component={Register} />
+                    <Route path="/login" component={Login} />
                 </Switch>
             </BrowserRouter>
         </React.Fragment>
